e1123478f7b462d8e4348f6ad4e3568390fac397
dakotawireless <erik@dakotawireless.net>
2026-09-20 01:36:13 -0600

    Enable pasted images in the chat composer

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./chat-layout-overrides.css";
+import "./image-paste.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
